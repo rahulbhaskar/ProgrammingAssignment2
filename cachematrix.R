9b530b014a05fca2c93a128689b237d8c9b7adf6
Rahul <rahulbhaskar1986@gmail.com>
2014-04-27 21:30:50 -0400

    Inverse computed
    The functions to compute the inverse of the matrix if it is not
    available in the cache have been written.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions return inverse of Matrix if it is in the cache, ## otherwise the inverse is computed and then returned
 
-## Write a short comment describing this function
+
+## makeCacheMatrix stores the value of inverse in Cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setInverse<-function(inverted) m<<-inverted
+getInverse<-function() m
+list (set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse if it is not available in ## cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getInverse()
+  
+  if(!is.null(m)){message("getting cached data")
+  return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setInverse(m)
+  m
 }
